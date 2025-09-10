524744be81f16a24ed2eec88833f1ef9f7c7f356
kabelo-ilab <Kabelo.tlhape@ilabqa.com>
2025-09-10 08:34:36 +0200

    Chapter 3 exercise completed.

--- JavaFundamentalsTraining/src/Chapter3/Exercises/RetirementGoal.java
+++ JavaFundamentalsTraining/src/Chapter3/Exercises/RetirementGoal.java
@@ -1,7 +1,15 @@
 package Chapter3.Exercises;
 
+import java.util.Scanner;
+
 public class RetirementGoal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int yearBeforeRetirement;
+        double amountToSave;
+
+        System.out.print("How many years before retirement? ");
+        yearBeforeRetirement = sc.nextInt();
 
     }
 }
